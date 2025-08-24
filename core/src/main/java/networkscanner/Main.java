b94b5bc571b9791d63ad6c9c2f10e7bfe6055f5c
Maddox-RVS <rynvnskl@yahoo.com>
2025-08-24 12:42:30 -0400

    Added UDP scanning.

--- core/src/main/java/networkscanner/Main.java
+++ core/src/main/java/networkscanner/Main.java
@@ -4,6 +4,9 @@ import java.net.InetAddress;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        NetworkScanner.quickPortScanTCP("scanme.nmap.org", 5000);
+        NetworkScanner.thoroughPortScan(
+            "scanme.nmap.org", 
+            5000, 
+            NetworkScanner.Protocol.UDP);
     }
 }
--- core/src/main/java/networkscanner/NetworkScanner.java
+++ core/src/main/java/networkscanner/NetworkScanner.java
@@ -9,13 +9,18 @@ import util.loading.Spinner;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import javax.sound.sampled.Port;
 
@@ -24,13 +29,19 @@ import util.TermInstructs;
 
 public class NetworkScanner {
     private static final int MAX_PORTS = 65535;
+    private static final int WAIT_DAYS = 1000;
+    private static final int UDP_THREAD_POOL_SIZE = 100;
+
+    public enum Protocol {
+        TCP,
+        UDP
+    }
 
     private record ServiceRecord(String serviceName, int port, double frequency, String comment) {}
 
     private record PortScanResult(ServiceRecord serviceRecord, boolean open) {}
 
     private static List<ServiceRecord> getNmapServicesTCP() throws IOException, StreamReadException, DatabindException {
-        // Load and parse nmap-services.json for TCP services
         List<ServiceRecord> serviceRecords = new ArrayList<>();
 
         InputStream in = NetworkScanner.class.getResourceAsStream("/nmap-services.json");
@@ -50,19 +61,34 @@ public class NetworkScanner {
         return serviceRecords;
     }
 
-    private static ServiceRecord[] getNmapServicesUDP() {
-        // Load and parse nmap-services.json for UDP services
-        return new ServiceRecord[0]; // Placeholder
+    private static List<ServiceRecord> getNmapServicesUDP() throws IOException, StreamReadException, DatabindException {
+        List<ServiceRecord> serviceRecords = new ArrayList<>();
+
+        InputStream in = NetworkScanner.class.getResourceAsStream("/nmap-services.json");
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Object> data = mapper.readValue(in, Map.class);
+        List<Map<String, Object>> tcpServices = (List<Map<String, Object>>) data.get("udp");
+        
+        for (Map<String, Object> service : tcpServices) {
+            String serviceName = (String) service.get("service name");
+            int port = (int) service.get("port");
+            double frequency = (double) service.get("frequency");
+            String comment = (String) service.get("comment");
+
+            serviceRecords.add(new ServiceRecord(serviceName, port, frequency, comment));
+        }
+
+        return serviceRecords;
     }
 
-    public static void quickPortScanTCP(String target, int timeout) {
+    public static void quickPortScan(String target, int timeout, Protocol protocol) {
         List<PortScanResult> scanResults = new ArrayList<>();
 
         try {
             Spinner spinner = new Spinner("Loading nmap-services data");
             spinner.start();
 
-            List<ServiceRecord> serviceRecords = getNmapServicesTCP();
+            List<ServiceRecord> serviceRecords = protocol == Protocol.TCP ? getNmapServicesTCP() : getNmapServicesUDP();
 
             spinner.stop();
 
@@ -71,138 +97,105 @@ public class NetworkScanner {
 
             System.out.printf(Colors.BLUE + "%-10s %-20s %-10s%n" + Colors.RESET, "Port", "Service", "Status");
 
-            List<Thread> threads = new ArrayList<>();
-
+            ExecutorService executor = protocol == Protocol.TCP ? Executors.newFixedThreadPool(serviceRecords.size()) : Executors.newFixedThreadPool(UDP_THREAD_POOL_SIZE);
             for (ServiceRecord serviceRecord : serviceRecords) {
-                Thread thread = new Thread() {
-                    @Override
-                    public void run() {
-                        boolean isOpen = connectTCP(target, serviceRecord.port(), timeout);
-                        scanResults.add(new PortScanResult(serviceRecord, isOpen));
+                executor.submit(() -> {
+                    boolean isOpen = protocol == Protocol.TCP ? connectTCP(target, serviceRecord.port(), timeout)
+                                                                : connectUDP(target, serviceRecord.port(), timeout);
+                    scanResults.add(new PortScanResult(serviceRecord, isOpen));
 
-                        System.out.printf("%-10d %-20s %-10s%n",
-                            serviceRecord.port(),
-                            serviceRecord.serviceName(),
-                            isOpen ? Colors.GREEN + "Open" + Colors.RESET : Colors.RED + "Closed" + Colors.RESET);
-                    }
-                };
-
-                thread.start();
-                threads.add(thread);
+                    System.out.printf("%-10d %-20s %-10s%n",
+                        serviceRecord.port(),
+                        serviceRecord.serviceName(),
+                        isOpen ? Colors.GREEN + "Open" + Colors.RESET : Colors.RED + "Closed" + Colors.RESET);
+                });
             }
+            executor.shutdown();
 
-            for (Thread thread : threads) {
-                try {
-                    thread.join();
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                }
+            try {
+                executor.awaitTermination(WAIT_DAYS, TimeUnit.DAYS);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
             }
         } catch (IOException e) {
             System.out.println(Colors.BG_RED + Colors.WHITE + "ERROR"
-                    + Colors.RESET + Colors.RED + "An issue occurred while reading in nmap-services data.");
+                    + Colors.RESET + Colors.RED + " An issue occurred while reading in nmap-services data." + Colors.RESET);
         }
 
     }
 
-    public static void quickPortScanUDP() {
-        // Implement UDP port scanning logic here
-    }
-
-    public static List<PortScanResult> thoroughPortScanTCP(String target, int timeout) {
+    public static List<PortScanResult> thoroughPortScan(String target, int timeout, Protocol protocol) {
         List<PortScanResult> scanResults = new ArrayList<>();
         
         try {
             Spinner spinner = new Spinner("Loading nmap-services data");
             spinner.start();
 
-            List<ServiceRecord> serviceRecords = getNmapServicesTCP();
+            List<ServiceRecord> serviceRecords = protocol == Protocol.TCP ? getNmapServicesTCP() : getNmapServicesUDP();
 
             spinner.stop();
 
             System.out.printf(Colors.BLUE + "%-10s %-20s %-10s%n" + Colors.RESET, "Port", "Service", "Status");
 
-            List<Thread> threads = new ArrayList<>();
-
+            ExecutorService executor = protocol == Protocol.TCP ? Executors.newFixedThreadPool(serviceRecords.size()) : Executors.newFixedThreadPool(UDP_THREAD_POOL_SIZE);
             for (ServiceRecord serviceRecord : serviceRecords) {
-                Thread thread = new Thread() {
-                    @Override
-                    public void run() {
-                        boolean isOpen = connectTCP(target, serviceRecord.port(), timeout);
-                        scanResults.add(new PortScanResult(serviceRecord, isOpen));
+                executor.submit(() -> {
+                    boolean isOpen = protocol == Protocol.TCP ? connectTCP(target, serviceRecord.port(), timeout) 
+                                                                : connectUDP(target, serviceRecord.port(), timeout);
+                    scanResults.add(new PortScanResult(serviceRecord, isOpen));
 
-                        System.out.printf("%-10d %-20s %-10s%n",
-                            serviceRecord.port(),
-                            serviceRecord.serviceName(),
-                            isOpen ? Colors.GREEN + "Open" + Colors.RESET : Colors.RED + "Closed" + Colors.RESET);
-                    }
-                };
-
-                thread.start();
-                threads.add(thread);
+                    System.out.printf("%-10d %-20s %-10s%n",
+                        serviceRecord.port(),
+                        serviceRecord.serviceName(),
+                        isOpen ? Colors.GREEN + "Open" + Colors.RESET : Colors.RED + "Closed" + Colors.RESET);
+                });
             }
+            executor.shutdown();
 
-            for (Thread thread : threads) {
-                try {
-                    thread.join();
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                }
+            try {
+                executor.awaitTermination(WAIT_DAYS, TimeUnit.DAYS);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
             }
         } catch (IOException e) {
             System.out.println(Colors.BG_RED + Colors.WHITE + "ERROR" 
-                                + Colors.RESET + Colors.RED + "An issue occurred while reading in nmap-services data.");
+                                + Colors.RESET + Colors.RED + " An issue occurred while reading in nmap-services data." + Colors.RESET);
             return new ArrayList<>();
         }
 
         return scanResults;
     }
 
-    public static List<PortScanResult> thoroughPortScanUDP(String target, int timeout) {
-        // Implement thorough UDP port scanning logic here
-        return new ArrayList<>();
-    }
-
-    public static List<PortScanResult> deepPortScanTCP(String target, int timeout) {
+    public static List<PortScanResult> deepPortScanTCP(String target, int timeout, Protocol protocol) {
         List<PortScanResult> scanResults = new ArrayList<>();
 
         System.out.printf(Colors.BLUE + "%-10s %-20s %-10s%n" + Colors.RESET, "Port", "Service", "Status");
 
-        List<Thread> threads = new ArrayList<>();
+        ExecutorService executor = protocol == Protocol.TCP ? Executors.newFixedThreadPool(MAX_PORTS) : Executors.newFixedThreadPool(UDP_THREAD_POOL_SIZE);
 
         for (int i = 0; i <= MAX_PORTS; i++) {
             final int port = i;
-            Thread thread = new Thread() {
-                @Override
-                public void run() {
-                    boolean isOpen = connectTCP(target, port, timeout);
-                    ServiceRecord serviceRecord = new ServiceRecord("Unknown", port, 0.0, "");
-                    scanResults.add(new PortScanResult(serviceRecord, isOpen));
+            executor.submit(() -> {
+                boolean isOpen = protocol == Protocol.TCP ? connectTCP(target, port, timeout) : connectUDP(target, port, timeout);
+                ServiceRecord serviceRecord = new ServiceRecord("Unknown", port, 0.0, "");
+                scanResults.add(new PortScanResult(serviceRecord, isOpen));
 
-                    System.out.printf("%-10d %-20s %-10s%n",
-                        port,
-                        serviceRecord.serviceName(),
-                        isOpen ? Colors.GREEN + "Open" + Colors.RESET : Colors.RED + "Closed" + Colors.RESET);
-                }
-            };
-
-            thread.start();
-            threads.add(thread);
+                System.out.printf("%-10d %-20s %-10s%n",
+                    port,
+                    serviceRecord.serviceName(),
+                    isOpen ? Colors.GREEN + "Open" + Colors.RESET : Colors.RED + "Closed" + Colors.RESET);
+            });
         }
 
-        for (Thread thread : threads) {
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
+        executor.shutdown();
+
+        try {
+            executor.awaitTermination(WAIT_DAYS, TimeUnit.DAYS);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
         }
 
         return scanResults;
-    }
-
-    public static void deepPortScanUDP() {
-        // Implement detailed UDP port scanning logic here
     }
 
     public static boolean connectTCP(String target, int port, int timeout) {
@@ -210,6 +203,32 @@ public class NetworkScanner {
             socket.connect(new InetSocketAddress(target, port), timeout);
             return true;
         } catch (IOException e) {
+            return false;
+        }
+    }
+
+    public static boolean connectUDP(String target, int port, int timeout) {
+        try {
+            DatagramSocket socket = new DatagramSocket();
+            socket.setSoTimeout(timeout);
+
+            byte[] sendData = "ping".getBytes();
+
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, InetAddress.getByName(target), port);
+            socket.send(sendPacket);
+
+            byte[] receiveData = new byte[1024];
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+
+            try {
+                socket.receive(receivePacket);
+                return true;
+            } catch (SocketTimeoutException e) {
+                return false;
+            }
+        } catch (IOException e) {
+            System.out.println(Colors.BG_RED + Colors.WHITE + "ERROR" 
+                                + Colors.RESET + Colors.RED + " An issue occurred while connecting to the target." + Colors.RESET);                                   
             return false;
         }
     }
